d54ac5dd8fb12284e1de4bec7ab2c0d42635aa23
sebastianbreuker <sebbebreuker@gmail.com>
2025-09-16 11:56:59 +0200

    Add project files.

---
+++ Labb 1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+Console.Write("Skriv in något: ");
+string input = Console.ReadLine();
+Console.WriteLine();
+long total = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (!char.IsDigit(input[i])) continue;
+    char bokstav = input[i];
+
+	for (int j = i + 1; j < input.Length; j++)
+	{
+        string tal = input.Substring(i, j - i + 1);
+        if (!char.IsDigit(input[j])) break;
+        if (input[j] != bokstav) continue;
+            
+            for (int k = 0; k < input.Length; k++)
+            {
+                 
+                if (k >= i && k <= j)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+
+                Console.Write(input[k]);
+
+            }
+            Console.WriteLine();
+        
+        total += long.Parse(tal);
+        break;
+	}
+    
+}
+
+Console.ResetColor();
+Console.WriteLine();
+Console.WriteLine($"Din totala summa är: {total}");
